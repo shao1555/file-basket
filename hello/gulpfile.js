0441180d39d8d8206fc3fa7999b3f66ce25ddeaa
Sho Sawada <shao@thebitcellar.com>
2016-02-12 18:45:15 +0900

    hello.es6

--- hello/gulpfile.js
+++ hello/gulpfile.js
@@ -4,7 +4,7 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('babel', function() {
-  return gulp.src('src/*.js')
+  return gulp.src('src/*.es6')
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(concat('all.js'))
